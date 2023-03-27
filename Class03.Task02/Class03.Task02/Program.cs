c3517389a8450399dd9745d25a9bd90a6d1e34ea
chrchevdj <118832611+chrchevdj@users.noreply.github.com>
2023-03-28 02:05:51 +0300

    Add files via upload

---
+++ Class03.Task02/Class03.Task02/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Class03.Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Make a new console application called StudentGroup
+            Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names (strings).
+            Get a number from the console between 1 and 2 and print the students from that group in the console.
+            Ex: studentsG1 ["Zdravko", "Petko", "Stanko", "Branko", "Trajko"]
+            Test Data:
+            Enter student group: ( there are 1 and 2 )
+            1
+            Expected Output:
+            The Students in G1 are:
+            Zdravko
+            Petko
+            Stanko
+            Branko
+            Trajko
+             */
+            string[] studentsG1 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsG2 = new string[] { "Colic", "Brus lee", "Vanco", "Gorgi", "Silvester" };
+
+            Console.WriteLine("Enter student group: ( there are 1 and 2 )");
+            int input = int.Parse(Console.ReadLine());
+
+            if(input != 1 || input != 2) 
+            {
+                Console.WriteLine("Please choose between 1 and 2");
+            }
+            if(input == 1)
+            {
+                foreach (string student in studentsG1)
+                {
+                    Console.WriteLine(student);
+                }
+                return;
+            }
+            if(input == 2)
+            {
+                foreach (string student in studentsG2)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+        }   
+    }
+}
---
+++ Class04.Task01/Class04.Task01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Class04.Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            if (input.Length < 5)
+            {
+                Console.WriteLine("The string is too short to print its last 5 characters.");
+            }
+            else
+            {
+                Console.WriteLine("The last 5 characters of the string are: " + input.Substring(input.Length - 5));
+            }
+        }
+    }
+}
