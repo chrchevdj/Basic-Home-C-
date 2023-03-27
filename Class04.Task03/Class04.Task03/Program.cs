b2dbd0e414e55c655609634e9b8aee5cde11b1a5
chrchevdj <118832611+chrchevdj@users.noreply.github.com>
2023-03-28 02:28:32 +0300

    Add files via upload

---
+++ Class04.Task03/Class04.Task03/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Class04.Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            // Calculate the sum of the digits in the number
+            int sum = 0;
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            // Print the sum of the digits
+            Console.WriteLine("The sum of the digits in the number is: " + sum);
+
+            // Prompt the user to enter a sentence
+            Console.Write("Enter a sentence: ");
+            string sentence = Console.ReadLine();
+
+            // Split the sentence into words
+            string[] words = sentence.Split(' ');
+
+            // Find the largest word
+            string largestWord = "";
+            foreach (string word in words)
+            {
+                if (word.Length > largestWord.Length)
+                {
+                    largestWord = word;
+                }
+            }
+
+            // Print the largest word
+            Console.WriteLine("The largest word in the sentence is: " + largestWord);
+        }
+    }
+}
