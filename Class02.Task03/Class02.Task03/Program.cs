da55b425f4ad8c8be95ebe7ea5eeb6a670232abb
chrchevdj <118832611+chrchevdj@users.noreply.github.com>
2023-03-28 02:02:51 +0300

    Add files via upload

---
+++ Class02.Task03/Class02.Task03/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Class02.Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Task 3:
+            Create new console application “SwapNumbers”. Input 2 numbers from the console and then swap the values of the variables so that the first variable has the second value and the second variable the first value. Please find example below:
+            Test Data:
+            Input the First Number: 5
+            Input the Second Number: 8
+            Expected Output:
+            After Swapping:
+            First Number: 8
+            Second Number: 5
+             */
+
+            Console.WriteLine("Input the First Number:");
+            int inputFirst = int.Parse(Console.ReadLine()); 
+            Console.WriteLine("Input the Second Number:");
+            int inputSecond = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Before");
+            Console.WriteLine($"First Number: {inputFirst}");
+            Console.WriteLine($"First Number: {inputSecond}");
+            Console.WriteLine("After");
+            Console.WriteLine($"First Number: {inputSecond}");
+            Console.WriteLine($"First Number: {inputFirst}");
+
+        }
+    }
+}
---
+++ Class03.Task01/Class03.Task01/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Class03.Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Task 1
+            Make a console application called SumOfEven. Inside it create an array of 6 integers. Find and print the sum of the even numbers from the array:
+            Test Data:
+            Enter integer no.1:
+            4
+            Enter integer no.1:
+            3
+            Enter integer no.1:
+            7
+            Enter integer no.1:
+            3
+            Enter integer no.1:
+            2
+            Enter integer no.1:
+            8
+            Expected Output:
+            The result is: 14
+             */
+
+            Console.WriteLine("SumOfEven");
+            int[] ints = new int[6];
+            Console.WriteLine("Enter integer no.1");
+            ints[0]= int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter integer no.2");
+            ints[1] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter integer no.3");
+            ints[2] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter integer no.4");
+            ints[3] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter integer no.5");
+            ints[4] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter integer no.6");
+            ints[5] = int.Parse(Console.ReadLine());
+
+            int result = 0;
+            for (int i = 0; i < ints.Length; i++)
+            {
+                if (ints[i] % 2 == 0)
+                result += ints[i];
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
