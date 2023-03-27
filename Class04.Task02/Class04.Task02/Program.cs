6c2abe13e0c756dcb4d952a2a9ca681387b4ed96
chrchevdj <118832611+chrchevdj@users.noreply.github.com>
2023-03-28 02:22:07 +0300

    Add files via upload

---
+++ Class04.Task02/Class04.Task02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Class04.Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a sentence: ");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+
+            Console.WriteLine("The words in the sentence are: ");
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
