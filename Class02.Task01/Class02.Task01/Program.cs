f24e40348951d3990847ed662959970798c72f0f
chrchevdj <118832611+chrchevdj@users.noreply.github.com>
2023-03-28 02:00:18 +0300

    Add files via upload

---
+++ Class02.Task01/Class02.Task01/Program.cs
@@ -0,0 +1,49 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System;
+
+namespace Class02.Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Task 1
+            Create new console application “RealCalculator” that takes two numbers from the input and asks what operation would the user want to be done( +, - , * , / ). Then it returns the result.
+            Test Data:
+            Enter the First number: 10
+            Enter the Second number: 15
+            Enter the Operation: +
+            Expected Output:
+            The result is: 25
+            */
+            Console.WriteLine("Enter the First number:");
+            int inputFirst = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second number:");
+            int inputSecond = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Operation: +, - , * , / ");
+            string inputOperation = Console.ReadLine();
+
+            int result = 0;
+            switch (inputOperation)
+            {
+                case "+":
+                    result = inputFirst + inputSecond;
+                    break;
+                case "-":
+                    result = inputFirst - inputSecond;
+                    break;
+                case "*":
+                    result = inputFirst * inputSecond;
+                    break;
+                case "/":
+                    result = inputFirst / inputSecond;
+                    break;
+
+            }
+            Console.WriteLine($"The result is: {result}");
+        }
+    }
+}
---
+++ Class02.Task02/Class02.Task02/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Class02.Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Task 2
+            Create new console application “AverageNumber” that takes four numbers as input to calculate and print the average.
+            Test Data:
+            Enter the First number: 10
+            Enter the Second number: 15
+            Enter the third number: 20
+            Enter the four number: 30
+            Expected Output:
+            The average of 10, 15, 20 and 30 is: 18
+             */
+
+            Console.WriteLine("Enter the First number:");
+            int inputFirst = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second number:");
+            int inputSecond = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the third number:");
+            int inputThird = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the four number:");
+            int inputFour = int.Parse(Console.ReadLine());
+
+            int result = ((inputFirst + inputSecond + inputThird + inputFour) / 4);
+            Console.WriteLine($"The average of{inputFirst} {inputSecond} {inputThird} {inputFour} is: {result}");
+
+
+        }
+    }
+}
